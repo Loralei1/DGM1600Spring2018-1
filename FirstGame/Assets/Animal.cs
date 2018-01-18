f71e28f2a9f5b9fc25d6ef1f4cf8df088877a1af
Meaghan Searle <msearle88@gmail.com>
2018-01-18 11:15:20 -0700

    Added animal class and call functions

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("My animal is eating.");
+		print("Now my animal is sleeping.");
+		print("I love my cat");
+		print("No, I really don't.");
+	}
+
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
