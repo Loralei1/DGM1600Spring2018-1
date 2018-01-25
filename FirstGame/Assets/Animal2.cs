8f2a85aa81279f59b90307953bb92611ca3f306f
Meaghan Searle <msearle88@gmail.com>
2018-01-25 11:18:42 -0700

    Created Cat, Dog, and Chameleon cube variables

---
+++ FirstGame/Assets/Animal2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal2 : MonoBehaviour {
+
+	public string Name;
+
+	public int FoodCount;
+
+	
+
+	void OnMouseDown()
+	{
+		print("I have a" + Name);
+		print("It has " + FoodCount + " cookies.");	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
