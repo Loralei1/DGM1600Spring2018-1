3d393a27fb802e1cce10be04799afb3119fa9dac
Meaghan Searle <msearle88@gmail.com>
2018-02-06 19:46:20 -0700

    Christmas Game edits

--- Second game/Assets/ClassNotes.cs
+++ Second game/Assets/ClassNotes.cs
@@ -8,7 +8,35 @@ public class ClassNotes : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+	//if(true); { 
+		//runcode
+		//} else {
+			//runothercode}
+		/* Else cannot be by itself. It depends on the if 
+		statement. You can put anything in the else clause. For example:
+	if( 3 + 1 == 4);{
+		print("The door is open.");
+	} else {
+		print("The way is shut. It was made by those who are
+		dead, and the dead keep it. The way is shut.");
+		}
+		
+		(In this case, 3 + 1 == 4 is always true, and the if-statement code will always 
+		run.)
+		I wish I had thought of taking notes like this ages ago.
+		
+		Operators a symbols that do funtions.
+		+ is add
+		- is subtract
+		/ is divide 
+		* is multiply
+		= assign value
+		== check equality
+		!= check for inequality
+		>=
+		>
+		<=
+		< */
 	// Update is called once per frame
 	void Update () {
 		
