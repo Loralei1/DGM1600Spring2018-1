e92bc1308fe1912a5dee209307ed4cf1ac011f2a
Meaghan Searle <msearle88@gmail.com>
2018-02-13 20:01:47 -0800

    Added more loops and arrays, comments

--- Second game/Assets/ArraysAndLoops.cs
+++ Second game/Assets/ArraysAndLoops.cs
@@ -8,6 +8,17 @@ public class ArraysAndLoops : MonoBehaviour {
 public int [] HighScores;
 public PowerUp[] PowerUps;
 public Player[] Players; 
+	public int[] Health;
+	public Weapon [] Weapons;
+	public int[] GoldPieces;
+	public float[] Speed;
+	public int [] Diamonds;
+	public Armor[] Armors;
+	public int [] Trinkets; 
+
+	/* The ten lines above are arrays. Arrays are declared by square brackets and hold a range of variables as large or as small 
+	as necessary, so long as those variables are of the same type. The PowerUps, Players, Weapons, and Armors arrays call to scriptable objects.
+	One of the benefits of using arrays is being able to edit variables without rewriting script; it can be done in Unity.*/
 
 	// Use this for initialization
 	void Start () {
@@ -25,6 +36,43 @@ public Player[] Players;
 			print(score);
 		}
 	}
+
+		foreach (var health in Health) {
+			if (health <= 0) {
+				print ("Game Over.");
+			}
+		}
+
+		foreach (var Weapon in Weapons) {
+			print (Weapon.WeaponName);
+		
+		}
+
+		foreach (var gold in GoldPieces) { 
+			print (gold);
+		}
+
+		foreach (var speed in Speed) {
+			if (speed >= 5) {
+				print ("Run");
+			}
+		}
+
+		foreach (var diamond in Diamonds) {
+			print (diamond);
+		}
+
+		foreach (var Armor in Armors) {
+			print (Armor.ArmorName);
+		}
+
+		foreach (var trinket in Trinkets) {
+			print (trinket);
+		}
+
+		/* The lines of code above are all foreach loops. They allow for work to be done with as many variables are contained in an array, and will do work for each variable in that array (or each variable in that
+		array which falls in the correct guidelines if an if-statement is included as part of the foreach loop). Foreach loops allow for huge amounts of work to be done without 
+		having to write a line of code for each and every variable. */
 	}
 
 }
