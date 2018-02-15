114bda0ff834c0586e48df6c96ae48c0bffdb980
Meaghan Searle <msearle88@gmail.com>
2018-02-15 11:22:54 -0700

    ListsandLoops Scene

---
+++ Second game/Assets/Scripts/ListsAndLoops.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ListsAndLoops : MonoBehaviour {
+ 
+public TheIncredibles TheIncredibles;
+public Text AddMember;
+
+// Lists are good for collecting things. 
+// Arrats are good for things you already have. 
+private void OnMouseDown()
+{
+	TheIncredibles.FamilyList.Add(AddMember.text);
+}
+}
+
+
