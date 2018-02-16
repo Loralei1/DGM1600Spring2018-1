b47132b8cfc827623939f9db78ae058196f1e497
Meaghan Searle <msearle88@gmail.com>
2018-02-15 20:23:35 -0800

    Added comments and examples

--- Second game/Assets/Scripts/ListsAndLoops.cs
+++ Second game/Assets/Scripts/ListsAndLoops.cs
@@ -9,7 +9,24 @@ public TheIncredibles TheIncredibles;
 public Text AddMember;
 
 // Lists are good for collecting things. 
-// Arrats are good for things you already have. 
+// Arrays are good for things you already have.
+/* Lists function similarly to arrays. The primary difference is that arrays are used for 
+   a collection of data in a single type intended to stay the same throughout a game, while lists
+   are used for a collection of data in a single type intended to change throuhout a game. 
+   A list is declared with enclosed carrots; for example,we could create a list in a script titled "Hobbits" that would go; public List<string> HobbitList; This list can be called to in another script in order to 
+   do work. So, in order to access the HobbitList script, we would write create public variables and write a function. For example:
+
+	public Hobbits Hobbits; 
+	public String AddHobbit;
+
+	private void Start ();
+
+	Hobbits.HobbitList.Add(AddMember.string);
+
+	Any data added during gameplay would be deleted once game play has stopped. In order to make additions permanent, we would have to create a scriptable object. 
+
+	Lists use the System.Collections.Generic; class. This has to be present in order for a list to function. */
+
 private void OnMouseDown()
 {
 	TheIncredibles.FamilyList.Add(AddMember.text);
