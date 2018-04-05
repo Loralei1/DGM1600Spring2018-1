d57976dad05d6f8551050fa5b3fef09c9dd44194
Meaghan Searle <msearle88@gmail.com>
2018-04-05 12:37:38 -0600

    MovePatternNotes

---
+++ New Unity Project 1/Assets/Death.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour 
+{
+	public PowerUp PowerUpTransfer;
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        
+       obj.GetComponent<MoveCharacter>().MovePattern = PowerUpTransfer.Transfer();
+    }
+}
