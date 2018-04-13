654612fd77c19403000839c8e0bd355b52fcc50b
Meaghan Searle <msearle88@gmail.com>
2018-04-13 12:55:09 -0700

    Figured out why the health bar wasn't reacting to anything.

--- Sidescroller/Assets/PowerUpandDownScripts/Trigger.cs
+++ Sidescroller/Assets/PowerUpandDownScripts/Trigger.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour {
 
 	public PowerUp PowerUpTransfer;
 	public GameObject other;
+	public Image HealthBar;
+	public float PowerLevel;
 
 	private void OnTriggerEnter (Collider obj)
+
 	{
+		HealthBar.fillAmount += PowerLevel;
 		obj.GetComponent<MoveCharacter> ().MovePattern = PowerUpTransfer.Transfer ();
 
 		if (other.gameObject.CompareTag ("Player")) {
