5b61940eb881283219a66755ffd555c2f5795a20
Meaghan Searle <msearle88@gmail.com>
2018-04-13 17:58:35 -0700

    Trying to get rocks to generate

---
+++ Sidescroller/Assets/RockDropScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDropScript : MonoBehaviour {}
+
+	/*public GameObject Rock; 
+	public Transform RockGenerationPoint; 
+	public float distanceBetween;
+	public float Gravity;
+
+	void Start()
+	{
+		Rock.GetComponent<SphereCollider> ();
+	}
+
+	void Update ()
+	{
+		if (transform.position.x < RockGenerationPoint.position.x) 
+		{
+			transform.position = new Vector3 (transform.position.x + distanceBetween, transform.position.y, transform.position.z);
+			Instantiate (Rock, transform.position, transform.rotation);
+		}	
+	}
+}*/
+	
+
