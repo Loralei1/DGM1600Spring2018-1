5fa10cd0c43062622495b655f54be08a36026e36
Meaghan Searle <msearle88@gmail.com>
2018-04-22 10:47:48 -0700

    Color Change Gate working

---
+++ Sidescroller/Assets/CoinScripts/CoinForLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinForLoop : MonoBehaviour {
+
+	public GameObject Rock; 
+	public float Gravity; 
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 12; i++) 
+		{
+			Instantiate(Rock);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
