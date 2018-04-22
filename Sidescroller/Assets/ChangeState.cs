6620d4d7207904aaebd185d8266eaea78dd91f34
Meaghan Searle <msearle88@gmail.com>
2018-04-22 06:58:34 -0700

    Thought power jump issue was fixed, turns out it's state machine

--- Sidescroller/Assets/ChangeState.cs
+++ Sidescroller/Assets/ChangeState.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ChangeState : StateMachineBehaviour {
 
-	/*public Player Player;
+	public Player Player;
 	public MovePattern MovePattern;
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		Player.MovePattern = MovePattern;*/
+		Player.MovePattern = MovePattern;
 	}
+}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
