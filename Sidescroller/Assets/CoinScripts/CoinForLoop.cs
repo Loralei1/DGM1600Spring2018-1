8d877dbd6ae9a53529201a28c467376602dfb515
Meaghan Searle <msearle88@gmail.com>
2018-04-22 11:04:25 -0700

    created a new rock drop script

--- Sidescroller/Assets/CoinScripts/CoinForLoop.cs
+++ Sidescroller/Assets/CoinScripts/CoinForLoop.cs
@@ -8,11 +8,14 @@ public class CoinForLoop : MonoBehaviour {
 	public float Gravity; 
 	// Use this for initialization
 	void Start () {
+
+		//while (player is moving){
 		for (int i = 0; i < 12; i++) 
 		{
 			Instantiate(Rock);
 		}
 	}
+	//}
 	
 	// Update is called once per frame
 	void Update () {
