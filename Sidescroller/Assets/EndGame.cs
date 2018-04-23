dd31bd7bbb836980e832360dd85b555be0326520
Meaghan Searle <msearle88@gmail.com>
2018-04-23 14:23:11 -0700

    EndGameTrigger

---
+++ Sidescroller/Assets/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	public Text YouWinText;
+
+
+	void Start ()
+	{
+		YouWinText.text = "";
+	}
+	private void OnTriggerEnter ()
+	{
+		YouWinText.text = "You Win!";
+	}
+}
