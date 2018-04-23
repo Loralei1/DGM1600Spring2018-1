36a46fbe47f091af2b1bc88add92ed36ee49aeeb
Meaghan Searle <msearle88@gmail.com>
2018-04-22 20:27:15 -0700

    Patrolling enemies set

---
+++ Sidescroller/Assets/Obstacles/Patrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour {
+
+	public Transform[] PatrolPoints;
+	private int destPoint = 0;
+	public NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start () {
+
+		agent = GetComponent<NavMeshAgent> ();
+		agent.autoBraking = false;
+		GoToNextPoint ();
+		
+	}
+
+	void GoToNextPoint ()
+	{
+		if (PatrolPoints.Length == 0)
+			return;
+		agent.destination = PatrolPoints[destPoint].position;
+		destPoint = (destPoint + 1) % PatrolPoints.Length;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!agent.pathPending && agent.remainingDistance < 0.5f) 
+		{
+			GoToNextPoint();
+		}
+		
+	}
+}
