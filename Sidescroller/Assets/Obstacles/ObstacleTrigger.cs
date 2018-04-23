3dbeb009bed05d273949f291c2d7a18992bca826
Meaghan Searle <msearle88@gmail.com>
2018-04-22 17:09:35 -0700

    Moving Enemy

---
+++ Sidescroller/Assets/Obstacles/ObstacleTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObstacleTrigger : MonoBehaviour {
+
+	public PowerUp PowerUpTransfer;
+	public GameObject other;
+	public Image HealthBar;
+	public float PowerLevel;
+	public GameObject Enemy;
+
+	private void OnTriggerEnter (Collider obj)
+
+	{
+		HealthBar.fillAmount += PowerLevel;
+		obj.GetComponent<MoveCharacter> ().MovePattern = PowerUpTransfer.Transfer ();
+
+		if (other.gameObject.CompareTag ("Player")) {
+			gameObject.SetActive (false);
+
+			for (int i = 0; i < 1; i++) 
+			{
+				Instantiate(Enemy);
+			}
+
+	}
+}
+}
