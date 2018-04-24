5ab2c37075b29bed7fceb02b6d05bc56e5fcaba5
Meaghan Searle <msearle88@gmail.com>
2018-04-24 11:33:48 -0700

    enum, Game states is on, doesn't do anything

--- Sidescroller/Assets/ChangeStatesAgain.cs
+++ Sidescroller/Assets/ChangeStatesAgain.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeState : StateMachineBehaviour {
+public class ChangeStatesAgain : StateMachineBehaviour {
 
-	public Player Player;
-	public MovePattern MovePattern;
+	public GameObject other;
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		Player.MovePattern = MovePattern;
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		//Player.MovePattern = MovePattern;
+
+			other.gameObject.SetActive (false);
+	
 	}
-}
+} // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
--- Sidescroller/Assets/Obstacles/EnemyCount.cs
+++ Sidescroller/Assets/Obstacles/EnemyCount.cs
@@ -10,24 +10,27 @@ public class EnemyCount : MonoBehaviour {
 	public Text HitText;
 	public GameObject other;
 	public GameObject AddMember;
+	private enum HitNumber {First, Second, Third, Fourth};
 	// Use this for initialization
 	void Start () 
 
 	{
 		HitsTaken = 0;
 		HitText.text = "Hits: ";
+		HitNumber MyHitNumber;
+		MyHitNumber = HitNumber.First;
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag ("Rock")) 
-		{
-			Enemies.Add(AddMember.gameObject);
+		if (other.gameObject.CompareTag ("Rock")) {
+			Enemies.Add (AddMember.gameObject);
 			foreach (GameObject Enemies in Enemies)
 				HitsTaken += 1;
 		}
 			
 	}
+
 	
 	// Update is called once per frame
 	void Update () 
