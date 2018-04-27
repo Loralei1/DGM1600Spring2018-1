a234f650ed65702fc242b237c79e4e456e1a024a
Meaghan Searle <msearle88@gmail.com>
2018-04-26 18:05:20 -0700

    Built sidescroller

---
+++ Sidescroller/Assets/Load.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour {
+
+	void Awake(){
+		Time.timeScale = 1f;
+	}
+
+	public void PlayGame () {
+		SceneManager.LoadScene(1);
+	}
+
+	public void LevelExit(){
+		if (Input.GetKey ("escape"))
+		{
+			Debug.Log ("Game has closed.");
+			Application.Quit ();
+		}
+	}
+}
