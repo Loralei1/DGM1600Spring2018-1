50e9a9eebfd1e1f6a942cc0efe9724e34a77714f
Meaghan Searle <msearle88@gmail.com>
2018-07-23 09:40:41 -0700

    Player movement for maze game

---
+++ New Unity Project (2)/Assets/InputZ.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InputZ : InputBase 
+
+
+{
+
+	public string InputName = "Vertical";
+	public float FloatValue = 5;
+	public override float MoveInput()
+	{
+		return Input.GetAxis (InputName);
+	}
+}
---
+++ New Unity Project (2)/Assets/JumpInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class JumpInput : InputBase {
+
+	public string InputName = "Jump";
+	public float FloatValue = 1;
+
+	public override float MoveInput()
+	{
+		return Input.GetButton (InputName) ? FloatValue : 0;
+	}
+
+
+}
+
+
